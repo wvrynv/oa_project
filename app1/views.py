dd45c5378d4d4b93b51fdf6e77c5c10f3aa10601
Zubaydullo Abdirakhmonov <z.abdirakhmonov@uic.group>
2022-02-23 01:10:40 +0500

    refactor: cookie

--- app1/views.py
+++ app1/views.py
@@ -1,29 +1,46 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core.mail import message, send_mail
 from django.shortcuts import render
 import datetime
+from django.utils import translation
 
 
 # Create your views here.
 
-def index (request):
-    response = render(request,'app1/index.html')
-    if not request.COOKIES.get('n_team'):
-        set_cookies(request,response)
+def index(request):
+    context = {
+        "cookie": request.COOKIES.get("lang", '')
+    }
+    print(request.COOKIES.get("lang"))
+    response = render(request, 'app1/index.html', context)
+    if not request.COOKIES.get('lang'):
+        set_cookies(request, response)
     return response
 
-def set_cookies (request, response):
-        max_age = 365 * 24 * 60 * 60  # 10 years
-        expires = datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age)
-        response.set_cookie('n_team', request.GET.get('n_team', ''),
-                    expires=expires.utctimetuple(), max_age=max_age)
-        
- 
-def contact_us (request):
 
+def set_cookies(request, response):
+    current_lang = translation.get_language()
+
+    max_age = 7 * 24 * 60 * 60  # one week 
+    expires = datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age)
+    response.set_cookie('lang', request.GET.get("lang", current_lang),
+                expires=expires.utctimetuple(), max_age=max_age)
+
+
+def delete_cookiee(request):
+    response = render(request, '/')
+    delete_cookies(request, response)
+    return response
+
+def delete_cookies(request, response):
+    response.set_cookie(request.GET.get("lang",''), expires=0, max_age=0)
+
+
+def contact_us (request):
+    
     if request.method == "POST":
 
         message_name = request.POST.get('message_name')
