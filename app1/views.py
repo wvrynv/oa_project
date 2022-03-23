ec7fc451a34f34a76705fca03126c4f98ecb5b13
Zubaydullo Abdirakhmonov <z.abdirakhmonov@uic.group>
2022-03-23 23:05:02 +0500

    refactor: cookie related code

--- app1/views.py
+++ app1/views.py
@@ -11,9 +11,8 @@ from django.utils import translation
 
 def index(request):
     context = {
-        "cookie": request.COOKIES.get("lang", '')
+        "cookie": request.COOKIES.get("lang")
     }
-    print(request.COOKIES.get("lang"))
     response = render(request, 'app1/index.html', context)
     if not request.COOKIES.get('lang'):
         set_cookies(request, response)
@@ -23,23 +22,34 @@ def index(request):
 def set_cookies(request, response):
     current_lang = translation.get_language()
 
-    max_age = 1  # one week 
+    max_age = 180  # one week
     expires = datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age)
     response.set_cookie('lang', request.GET.get("lang", current_lang),
                 expires=expires.utctimetuple(), max_age=max_age)
 
 
-def delete_cookiee(request):
-    response = render(request, '/')
-    delete_cookies(request, response)
-    return response
+# def delete_cookiee(request):
+#     response = render(request, '/')
+#     delete_cookies(request, response)
+#     return response
+#
+# # cookie func
+# def delete_cookies(request, response):
+#     response.set_cookie(request.GET.get("lang",''), expires=0, max_age=0)
 
-# cookie func
-def delete_cookies(request, response):
-    response.set_cookie(request.GET.get("lang",''), expires=0, max_age=0)
+from django.http import HttpResponseRedirect
+
+def delete(request):
+    response = render(request, 'app1/index.html')
+    delete_cookie(request, response)
+    return HttpResponseRedirect('index')
 
 
-def contact_us (request):
+def delete_cookie(request, response):
+    response.set_cookie(max_age=0, expires='Thu, 01-Jan-1970 00:00:00 GMT')
+
+
+def contact_us(request):
     
     if request.method == "POST":
         message_name = request.POST.get('message_name')
@@ -62,7 +72,13 @@ def contact_us (request):
         return render(request, 'app1/contact_us.html', {})
 
 def projects (request):
-    return render(request, 'app1/projects.html')
+    context = {
+        "cookie": request.COOKIES.get("lang")
+    }
+    response = render(request, 'app1/projects.html', context)
+    if not request.COOKIES.get('lang'):
+        set_cookies(request, response)
+    return response
 
 def career (request):
     return render(request, 'app1/career.html')
